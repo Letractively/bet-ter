7ad308b62a85de5c3ff9b77e8bcde7391526b883
doncorso <doncorso@gmail.com>
2014-07-02 16:43:06 +0000

    .... korrigiert

--- connect.inc.php
+++ connect.inc.php
@@ -1,8 +1,8 @@
 <?php 
-$dbHost = "rdbms.strato.de";
-$dbUser = "U1639187";
-$dbPass = "m5b!3ck4Li68tyx";
-$dbName = "DB1639187";
+$dbHost = "";
+$dbUser = "";
+$dbPass = "";
+$dbName = "";
 $connect = @mysql_connect($dbHost, $dbUser, $dbPass) or die("Konnte keine Verbindung zum Datenbankserver aufbauen!");
 $selectDB = @mysql_select_db($dbName, $connect) or die("Konnte die Datenbank <b></b> nicht auswählen!");
 ?>
